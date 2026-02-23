3ddd52129e390e52985fd8115796d2fe5fae5f78
Bark-Worthy <barkworthy.shop@gmail.com>
2026-02-23 18:03:40 +0800

    added fact_orders and fact_orders_line_items

---
+++ bark_warehouse/models/marts/orders/fact_orders.sql
@@ -0,0 +1,16 @@
+select      order_id,
+			order_source,
+			order_date,
+			customer_name,
+			cancel_date,
+			sum(quantity) filter (where purchase_type = 'sku') 					as purchased_quantity,
+			count(purchase_id) filter (where purchase_type = 'sku') 			as num_purchase_types,
+			sum(net_revenue) filter (where purchase_type = 'sku') 				as total_net_revenue,
+			coalesce(sum(quantity) filter (where purchase_type = 'freebie'),0)	as freebies_quantity,
+			count(purchase_id) filter (where purchase_type = 'freebie') 		as num_freebie_types
+from        {{ ref('fact_orders_line_items') }}
+group by 	order_id,
+			order_source,
+			order_date,
+			customer_name,
+			cancel_date
