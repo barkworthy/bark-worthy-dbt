f3eeef03a5f26aad8d4c92d797a85f43db0e822a
Bark-Worthy <barkworthy.shop@gmail.com>
2026-02-25 17:53:48 +0800

    added is_new_order to fact_orders

--- bark_warehouse/models/marts/orders/fact_orders.sql
+++ bark_warehouse/models/marts/orders/fact_orders.sql
@@ -1,16 +1,37 @@
-select      order_id,
-			order_source,
-			order_timestamp,
-			customer_name,
-			cancel_date,
-			sum(quantity) filter (where purchase_type = 'sku') 					as purchased_quantity,
-			count(purchase_id) filter (where purchase_type = 'sku') 			as num_purchase_types,
-			sum(net_revenue) filter (where purchase_type = 'sku') 				as total_net_revenue,
-			coalesce(sum(quantity) filter (where purchase_type = 'freebie'),0)	as freebies_quantity,
-			count(purchase_id) filter (where purchase_type = 'freebie') 		as num_freebie_types
-from        {{ ref('fact_orders_line_items') }}
-group by 	order_id,
-			order_source,
-			order_timestamp,
-			customer_name,
-			cancel_date
+with
+
+base as (
+	select      order_id,
+				order_source,
+				order_timestamp,
+				customer_name,
+				cancel_date,
+				sum(quantity) filter (where purchase_type = 'sku') 					as purchased_quantity,
+				count(purchase_id) filter (where purchase_type = 'sku') 			as num_purchase_types,
+				sum(net_revenue) filter (where purchase_type = 'sku') 				as total_net_revenue,
+				coalesce(sum(quantity) filter (where purchase_type = 'freebie'),0)	as freebies_quantity,
+				count(purchase_id) filter (where purchase_type = 'freebie') 		as num_freebie_types
+	from        {{ ref('fact_orders_line_items') }}
+	group by 	order_id,
+				order_source,
+				order_timestamp,
+				customer_name,
+				cancel_date
+),
+
+is_repeat_order as (
+	select 		order_id,
+				order_source,
+				customer_name,
+				case
+					when row_number() over (partition by order_source, customer_name order by order_id) = 1 then TRUE
+					else FALSE
+				end as is_new_order
+	from 		base
+	where 		cancel_date is null
+)
+
+select 		b.*,
+			r.is_repeat_order
+from 		base b
+			left join is_repeat_order r on b.order_id = r.order_id and r.order_source = b.order_source
