9c11efe027113fb954184d43a7bd4a9f51fd051c
Bark-Worthy <barkworthy.shop@gmail.com>
2026-03-15 02:08:14 +0800

    added int_order_line_items_base, removed order_freebies, added columns

--- bark_warehouse/models/intermediate/orders/int_order_line_items.sql
+++ bark_warehouse/models/intermediate/orders/int_order_line_items.sql
@@ -1,25 +1,6 @@
 select 		order_id,
-			'shopee' as order_source,
-			ordered_date::timestamp as ordered_timestamp,
-			ordered_date as paid_date,
-			customer_username as customer_name,
+			order_source,
 			purchase_id,
 			purchase_type,
-			quantity,
-			net_revenue,
-			null as cancelled_date
-from 		{{ ref('stg_google_sheets__shopee_sales') }}
-
-union all
-
-select 		order_id,
-			'direct' as order_source,
-			ordered_timestamp,
-			paid_date,
-			customer_name,
-			purchase_id,
-			purchase_type,
-			quantity,
-			price_at_purchase as net_revenue,
-			cancelled_date
-from 		{{ ref('stg_google_sheets__direct_sales') }}
+			quantity
+from 		{{ ref('int_order_line_items_base') }}
