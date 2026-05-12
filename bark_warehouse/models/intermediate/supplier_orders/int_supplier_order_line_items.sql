767d40e3954c1fbd751a864b18ac850673dbf160
Bark-Worthy <barkworthy.shop@gmail.com>
2026-05-12 22:52:09 +0800

    supplier_tables

---
+++ bark_warehouse/models/intermediate/supplier_orders/int_supplier_order_line_items.sql
@@ -0,0 +1,24 @@
+with
+
+order_total as (
+	select 	order_id,
+			order_total_cost_php,
+			order_total_cost_original
+	from 	{{ ref('int_supplier_orders') }}
+)
+
+select 		o.order_id,
+			o.purchase_name,
+			o.order_quantity,
+			o.order_unit,
+			o.unit_net_weight_g,
+			o.original_order_total_cost as original_item_total_cost,
+			o.original_order_currency,
+			ot.order_total_cost_original,
+			(o.original_order_total_cost / ot.order_total_cost_original) * order_total_cost_php as item_total_cost_php,
+			(o.original_order_total_cost / ot.order_total_cost_original) * order_total_cost_php / order_quantity as item_unit_cost_php,
+			ot.order_total_cost_php,
+			o.rejects,
+			o.notes
+from 		{{ ref('stg_google_sheets__supplier_order_line_items') }} o
+			left join order_total ot on o.order_id = ot.order_id
